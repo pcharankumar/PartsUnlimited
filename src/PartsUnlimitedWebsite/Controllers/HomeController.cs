770865846733b639d7f52ca548e4588f2ee105a2
Charan <pkumar@primesoft.net>
2018-08-30 00:42:10 -0700

    Recom Changes

--- src/PartsUnlimitedWebsite/Controllers/HomeController.cs
+++ src/PartsUnlimitedWebsite/Controllers/HomeController.cs
@@ -81,74 +81,8 @@ public ActionResult Recomendations()
 
             return View();
         }
-
-        public ActionResult Recomendation2()
-        {
-            ViewBag.Message = "Your application description page.";
-            //See file /home/Recomendations.cshtml for initial rendering
-
-            // Group the order details by product and return
-            // the products the top recomendations for the recomendations page
-
-            int count = 0;
-            while (count < roco_count
-                   )
-            {
-                _db.Products
-                    .OrderByDescending(a => a.OrderDetails.Count())
-                    .Take(count++)
-                    .ToList();
-            }
-
-
-            return View();
-        }
-
-        public ActionResult Recomendation3()
-        {
-            ViewBag.Message = "Your application description page.";
-            //See file /home/Recomendations.cshtml for initial rendering
-
-            // Group the order details by product and return
-            // the products the top recomendations for the recomendations page
-
-            int count = 0;
-            while (count < roco_count
-                   )
-            {
-                _db.Products
-                    .OrderByDescending(a => a.OrderDetails.Count())
-                    .Take(count++)
-                    .ToList();
-            }
-
-
-            return View();
-        }
-
-        public ActionResult Recomendation11()
-        {
-            ViewBag.Message = "Your application description page.";
-            //See file /home/Recomendations.cshtml for initial rendering
-
-            // Group the order details by product and return
-            // the products the top recomendations for the recomendations page
-
-            int count = 0;
-            while (count < roco_count
-                   )
-            {
-                _db.Products
-                    .OrderByDescending(a => a.OrderDetails.Count())
-                    .Take(count++)
-                    .ToList();
-            }
-
-
-            return View();
-        }
-
-        public ActionResult Recomendation12()
+        
+        public ActionResult Recomendation13()
         {
             ViewBag.Message = "Your application description page.";
             //See file /home/Recomendations.cshtml for initial rendering
