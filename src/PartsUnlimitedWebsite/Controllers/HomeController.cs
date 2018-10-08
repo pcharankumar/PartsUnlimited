f818f4b93499a6aab3bb0c6ea641ff275fe70736
Charan <pkumar@primesoft.net>
2018-10-07 23:55:35 -0700

    08102018-7

--- src/PartsUnlimitedWebsite/Controllers/HomeController.cs
+++ src/PartsUnlimitedWebsite/Controllers/HomeController.cs
@@ -82,51 +82,7 @@ public ActionResult Recomendations()
             return View();
         }
 
-        public ActionResult Recomendations306()
-        {
-            ViewBag.Message = "Your application description page.";
-            //See file /home/Recomendations.cshtml for initial rendering
-
-            // Group the order details by product and return
-            // the products the top recomendations for the recomendations page
-
-            int count = 0;
-            while (count < roco_count
-                   )
-            {
-                _db.Products
-                    .OrderByDescending(a => a.OrderDetails.Count())
-                    .Take(count++)
-                    .ToList();
-            }
-
-
-            return View();
-        }
-
-        public ActionResult Recomendations307()
-        {
-            ViewBag.Message = "Your application description page.";
-            //See file /home/Recomendations.cshtml for initial rendering
-
-            // Group the order details by product and return
-            // the products the top recomendations for the recomendations page
-
-            int count = 0;
-            while (count < roco_count
-                   )
-            {
-                _db.Products
-                    .OrderByDescending(a => a.OrderDetails.Count())
-                    .Take(count++)
-                    .ToList();
-            }
-
-
-            return View();
-        }
-
-        public ActionResult Recomendations308()
+        public ActionResult Recomendations309()
         {
             ViewBag.Message = "Your application description page.";
             //See file /home/Recomendations.cshtml for initial rendering
