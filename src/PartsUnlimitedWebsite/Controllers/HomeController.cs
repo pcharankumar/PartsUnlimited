cdb19dbd7b87024a2a5dde67264bf9576d6393d6
Charan <pkumar@primesoft.net>
2018-10-29 05:50:21 -0700

    123654

--- src/PartsUnlimitedWebsite/Controllers/HomeController.cs
+++ src/PartsUnlimitedWebsite/Controllers/HomeController.cs
@@ -81,140 +81,28 @@ public ActionResult Recomendations()
 
             return View();
         }
-
-        public ActionResult Recomendations331()
-        {
-            ViewBag.Message = "Your application description page.";
-            //See file /home/Recomendations.cshtml for initial rendering
-
-            // Group the order details by product and return
-            // the products the top recomendations for the recomendations page
-
-            int count = 0;
-            while (count < roco_count
-                   )
-            {
-                _db.Products
-                    .OrderByDescending(a => a.OrderDetails.Count())
-                    .Take(count++)
-                    .ToList();
-            }
-
-
-            return View();
-        }
-
-        public ActionResult Recomendations332()
-        {
-            ViewBag.Message = "Your application description page.";
-            //See file /home/Recomendations.cshtml for initial rendering
-
-            // Group the order details by product and return
-            // the products the top recomendations for the recomendations page
-
-            int count = 0;
-            while (count < roco_count
-                   )
-            {
-                _db.Products
-                    .OrderByDescending(a => a.OrderDetails.Count())
-                    .Take(count++)
-                    .ToList();
-            }
-
-
-            return View();
-        }
-
-        public ActionResult Recomendations333()
-        {
-            ViewBag.Message = "Your application description page.";
-            //See file /home/Recomendations.cshtml for initial rendering
-
-            // Group the order details by product and return
-            // the products the top recomendations for the recomendations page
-
-            int count = 0;
-            while (count < roco_count
-                   )
-            {
-                _db.Products
-                    .OrderByDescending(a => a.OrderDetails.Count())
-                    .Take(count++)
-                    .ToList();
-            }
-
-
-            return View();
-        }
-
-        public ActionResult Recomendations335()
-        {
-            ViewBag.Message = "Your application description page.";
-            //See file /home/Recomendations.cshtml for initial rendering
-
-            // Group the order details by product and return
-            // the products the top recomendations for the recomendations page
-
-            int count = 0;
-            while (count < roco_count
-                   )
-            {
-                _db.Products
-                    .OrderByDescending(a => a.OrderDetails.Count())
-                    .Take(count++)
-                    .ToList();
-            }
-
-
-            return View();
-        }
-        public ActionResult Recomendations336()
-        {
-            ViewBag.Message = "Your application description page.";
-            //See file /home/Recomendations.cshtml for initial rendering
-
-            // Group the order details by product and return
-            // the products the top recomendations for the recomendations page
-
-            int count = 0;
-            while (count < roco_count
-                   )
-            {
-                _db.Products
-                    .OrderByDescending(a => a.OrderDetails.Count())
-                    .Take(count++)
-                    .ToList();
-            }
-
-
-            return View();
-        }
-
-        public ActionResult Recomendations337()
-        {
-            ViewBag.Message = "Your application description page.";
-            //See file /home/Recomendations.cshtml for initial rendering
-
-            // Group the order details by product and return
-            // the products the top recomendations for the recomendations page
-
-            int count = 0;
-            while (count < roco_count
-                   )
-            {
-                _db.Products
-                    .OrderByDescending(a => a.OrderDetails.Count())
-                    .Take(count++)
-                    .ToList();
-            }
-
-
-            return View();
-        }
-
         //stubbing in a recomendations action
+        public ActionResult Recomendations338()
+        {
+            ViewBag.Message = "Your application description page.";
+            //See file /home/Recomendations.cshtml for initial rendering
 
+            // Group the order details by product and return
+            // the products the top recomendations for the recomendations page
+
+            int count = 0;
+            while (count < roco_count
+                   )
+            {
+                _db.Products
+                    .OrderByDescending(a => a.OrderDetails.Count())
+                    .Take(count++)
+                    .ToList();
+            }
+
+
+            return View();
+        }
 
         public void demos() { }
         private List<Product> GetNewProducts(int count)
